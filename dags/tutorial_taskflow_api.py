4b5623619948c0f77a5be981d4103ce08129800f
Alex <ajaimepozas@gmail.com>
2025-10-29 18:52:33 -0600

    taskflow tutorial

--- dags/tutorial_taskflow_api.py
+++ dags/tutorial_taskflow_api.py
@@ -33,6 +33,7 @@ def tutorial_taskflow_api():
 
         order_data_dict = json.loads(data_string)
         return order_data_dict
+
     @task(multiple_outputs=True)
     def transform(order_data_dict: dict):
         """
@@ -46,6 +47,7 @@ def tutorial_taskflow_api():
             total_order_value += value
 
         return {"total_order_value": total_order_value}
+
     @task()
     def load(total_order_value: float):
         """
@@ -58,4 +60,5 @@ def tutorial_taskflow_api():
     order_data = extract()
     order_summary = transform(order_data)
     load(order_summary["total_order_value"])
+    
 tutorial_taskflow_api()
